4d2e720f69303d53c75ebbaae84cf2bac7b08dbd
ChenYongYang <1214137301@qq.com>
2018-09-07 14:03:07 +0800

    解决移动端适配问题

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+import 'normalize.css' // 相当于reset.css
+import 'lib-flexible' // 需要配套px2rem-loader解析
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
